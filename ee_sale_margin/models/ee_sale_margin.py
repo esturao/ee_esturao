3164d35f2a0dd761ad15ba92c8d81df85a9bbfdf
esturao <esturao@esturao.com>
2020-04-08 09:33:52 +0200

    1

--- ee_sale_margin/models/ee_sale_margin.py
+++ ee_sale_margin/models/ee_sale_margin.py
@@ -5,13 +5,9 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
     
     hide_margin =fields.Boolean("Hide margin", default=True, help='Hide or show sales margin as needed.')
-    
-    @api.onchange('hide_margin') # if these fields are changed, call method
-    def check_change(self):
-        for line in self.order_line:
-            line.hide_margin = self.hide_margin
+
     
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    hide_margin =fields.Boolean("Hide margin", default=True, help='Hide or show sales margin as needed.')
+    hide_margin = fields.Boolean(related='order_id.hide_margin')
